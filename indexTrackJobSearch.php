1ad50107e798e2a3d70148f927e54bc437a32851
Mike K <mkem50@gmail.com>
2016-07-23 22:54:17 -0700

    added changes to fix job form toggle

--- indexTrackJobSearch.php
+++ indexTrackJobSearch.php
@@ -13,41 +13,39 @@ $server = mysql_connect("localhost","root", "test");
 $db =  mysql_select_db("test",$server);
 $query = mysql_query("select * from job_opportunities");
 
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Insert Data into MySQL DB</title>
-	<!--Javascript-->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <title>Insert Data into MySQL DB</title>
+    <!--Javascript-->
+    <!-- // <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <!-- // <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script> -->
 
-	 <!--Css Style Sheet-->
-	 <link rel="stylesheet" href="./css/style.css">
-	 <link rel="stylesheet" href="./css/jobInfoStyle.css">
+     <!--Css Style Sheet-->
+     <link rel="stylesheet" href="./css/style.css">
+     <link rel="stylesheet" href="./css/jobInfoStyle.css">
+      <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css"> -->
 
 </head>
 
 <body>
-<section class="login">
-    <div class="titulo">Add a Job</div>
-    <form action="#" method="post" enctype="application/x-www-form-urlencoded">
-        <div class="insertSection">
-        <input type="text" oninvalid="invalidJobIdFunct(this);" required title="Job Id required" placeholder="Job Id" data-icon="U">
-        <input type="text" oninvalid="invalidCompanyFunct(this);" required title="Company required" placeholder="Company" data-icon="x">
-        <input type="text" placeholder="Position" data-icon="U">
-        <input type="text" placeholder="Description" data-icon="x">
-        <input type="text" placeholder="Status" data-icon="U">
-        <input type="text" placeholder="Recruiter/Staffing Agency" data-icon="x">
+<button id="button">Run Effect</button>
 
-        <!-- <div class="olvido">
-            <div class="col"><a href="#" title="Ver Carásteres">Register</a></div>
-            <div class="col"><a href="#" title="Recuperar Password">Fotgot Password?</a></div>
-        </div>
-        <a href="#" class="enviar">Submit</a> -->
+
+
+<section id="loginId" class="login">
+    <div class="titulo">Add a Job</div>
+        <form action="#" method="post" enctype="application/x-www-form-urlencoded">
+            <div class="insertSection">
+            <input type="text" oninvalid="invalidJobIdFunct(this);" required title="Job Id required" placeholder="Job Id" data-icon="U">
+            <input type="text" oninvalid="invalidCompanyFunct(this);" required title="Company required" placeholder="Company" data-icon="x">
+            <input type="text" placeholder="Position" data-icon="U">
+            <input type="text" placeholder="Description" data-icon="x">
+            <input type="text" placeholder="Status" data-icon="U">
+            <input type="text" placeholder="Recruiter/Staffing Agency" data-icon="x">
 
     <div id="buttonDiv">
         <button id ="mySave" class="myButton">Save</button>
@@ -58,6 +56,7 @@ $query = mysql_query("select * from job_opportunities");
 </section>
 
 
+<div id="tableDiv">
 <!--Table-->
 <table id="myTable" cellspacing='0'> 
     <!-- cellspacing='0' is important, must stay -->
@@ -93,32 +92,25 @@ $query = mysql_query("select * from job_opportunities");
     echo "</tr>";
 }
 ?>
-
-        
-
-       <!--  <tr>
-            <td><a href="#yep-iit-doesnt-exist">Hyperlink   Example</a></td>
-            <td>80%</td>
-            <td><a href="#inexistent-id">Another</a></td>
-        </tr> -->
-
-
-
     </tbody>
-    <!-- Table Body -->
-
 </table>
-
-
-
-
-
-
+</div>
 <script>
+
 $("#myClear").click(function(){
     $(".insertSection input[type='text']").val(""); 
 });
 
+function invalidJobIdFunct(x){
+x.setCustomValidity('Please enter a Job Id');
+}
+function invalidCompanyFunct(x){
+x.setCustomValidity('Please enter company name');
+}
+
+ $("#button").click(function(){
+        $("#loginId").toggle();
+   });
 
 
 
